ecc8deaeed17f593704b7dffff4a1cc2e3a5bffe
michaeldacanay <michaeldacanayusa@gmail.com>
2022-12-21 18:19:45 -0500

    Add .gitignore and Set up Storyboard Layout and connect Storyboard elements to ViewController

--- IOS102 Pre-work: Introduce Yourself/ViewController.swift
+++ IOS102 Pre-work: Introduce Yourself/ViewController.swift
@@ -9,6 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    
+    @IBAction func introduceSelfDidTapButton(_ sender: UIButton) {
+        // Let's us choose the title we have selected from the segmented control
+        // In our example that is whether it is first, second, third or fourth
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+        // Creating a constant of type string that holds an introduction. The introduction receives the values from the outlet connections.
+        let introduction = """
+            My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets.
+            """
+        
+        print(introduction)
+    }
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        numberOfPetsLabel.text = "\(Int(sender.value))"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
